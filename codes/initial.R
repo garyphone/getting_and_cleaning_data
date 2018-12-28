1a8e3989e643250f43356a32c6cec1ff8a90de38
Gary Phone <otto.gao24@gmail.com>
2018-12-28 17:02:16 +0100

    Update initial.R

--- codes/initial.R
+++ codes/initial.R
@@ -2,10 +2,26 @@
 
 # location of the current working directory as an absolute path
 
-# Sets a working directory to the location of the current script
-# Params:
+# set a working directory to the location of the current script
+# parameters:
 # dir .. the root directory of the script considered the 'current' one
 set_wd <- function(dir) {
         setwd(file.path(this.dir, dir))
+}
+
+# resume the working directory to the original location.
+# this method is supposed to be called at the end of the client script
+resume_wd <- function() {
+        setwd(this.dir)
+}
+
+# install the required packages if needed and imports them
+load_packages <- function(...) {
+        packages <- c(...)
+        lapply(packages, function(pkg) {
+                if (!pkg %in% installed.packages()) 
+                        install.packages(pkg)
+        })
+        sapply(packages, require, character.only = TRUE, quietly = TRUE)
 }
 
